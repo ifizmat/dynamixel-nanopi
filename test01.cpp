201a171634fab7a950684149bd6dd18ec234b2c1
ifizmat <KasatkinKA_ggpi@mail.ru>
2021-05-19 04:22:39 +0000

    Test open port, get baudrate.

--- test01.cpp
+++ test01.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <iomanip>
+#include "dynamixel_sdk.h"
+
+#define DEVICENAME "/dev/ttyS2"
+
+dynamixel::PortHandler *portHandler;
 
 int main() {
   using namespace std;
-  cout << "\nTest 01. Work by Firstname Secondname!;16m\n";
+  cout << "\nTest 01. Work by Firstname Secondname!\n";
+  
+  portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+  cout << "portHandler: " << portHandler << endl;
+  
+  bool isPortOpened = portHandler->openPort();
+  cout << "Port: " << DEVICENAME << endl;
+  cout << "Is Port Opened: " << boolalpha << isPortOpened << endl;
+  
+  int baudRate = portHandler->getBaudRate();
+  cout << "Baudrate: " << baudRate << endl;
   return 0;
 }
