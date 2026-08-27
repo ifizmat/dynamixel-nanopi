f08e6b4ae531762a45d4530846b979480c784bd0
ifizmat <KasatkinKA_ggpi@mail.ru>
2026-08-27 10:27:56 +0400

    Set mode RS485 through ioctl from module fcntl, for serial set DTR, RTS. Received packet from servo, but incorrect structure.

--- test_led.py
+++ test_led.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import fcntl
+import struct
 from dynamixel_sdk import *
 
 BAUDRATE = 57600
@@ -8,7 +10,7 @@ DEVICENAME = '/dev/ttyS2'
 PROTOCOL_VERSION = 2.0
 
 # Servo Dynamixel XL430-W250
-SERVO_ID = 0X02
+SERVO_ID = 0X01
 LED_OFF = 0
 LED_ON  = 1
 LED_BLINK_COUNT = 4
@@ -24,25 +26,54 @@ is_port_opened = False
 
     
 def main():
+    global is_port_opened
     print("Work by Firstname Secondname!")
     portHandler = PortHandler(DEVICENAME)
-    portHandler.setBaudRate(BAUDRATE)
     packetHandler = PacketHandler(PROTOCOL_VERSION)
     
     is_port_opened = portHandler.openPort()
+
+    if is_port_opened:
+        portHandler.setBaudRate(BAUDRATE)
+        # Clearly down DTR/RTS
+        portHandler.ser.dtr = False        
+        portHandler.ser.rts = False        
+
+        # Set mode RS485 through ioctl
+        TIOCSRS485 = 0x542F
+        rs485_struct = struct.pack('IIIIIIII', 0X0001, 0, 0, 0, 0, 0, 0, 0)
+        try:
+            fcntl.ioctl(portHandler.ser.fd, TIOCSRS485, rs485_struct)
+            print(f'[OK] RS485 mode enabled')
+        except Exception as e:
+            print(f'[WARNING] Failed to set RS485 mode: {e}')
+        
     
     port_debug_info(portHandler)    
+    led_debug_info('LED Off error code: ', portHandler, packetHandler)
     portHandler.closePort()
 
 
+def led_debug_info(message, portHandler, packetHandler):
+    dxl_error = 0
+    status_led = 0
+    dxl_comm_result = 0
+    status_led, dxl_comm_result, dxl_error = packetHandler.read1ByteTxRx(portHandler, SERVO_ID, SERVO_REG_LED_STATUS)
+    print(f'LED status: {status_led}')
+    print(f'message {dxl_error}')
+    print(f'LED status reading communication result: {dxl_comm_result}')
+    print(f'Result: {packetHandler.getTxRxResult(dxl_comm_result)}')
+    
+    
 def port_debug_info(portHandler):
     print(f'portHandler: {portHandler}')
     print(f'Port: {DEVICENAME}')
     print(f'Is Port Opened: {is_port_opened}')
     baudrate = portHandler.getBaudRate()
     print(f'Baudrate: {baudrate}')
+    print(f'DTR: {portHandler.ser.dtr}')
+    print(f'RTS: {portHandler.ser.rts}')
 
 
 if __name__ == "__main__":
     main()
-    
